765e1850bb14e7d5bf641171a18e9092bfc73232
tuan <rickytuan1991@gmail.com>
2022-08-15 14:27:25 +0300

    variable

--- Practice_lessons1508/variables/variables_01.js
+++ Practice_lessons1508/variables/variables_01.js
@@ -1,4 +1,10 @@
-'use strict';
+"use strict";
 
 //define a variable "count" with value 0
 // Define a variable called count with an original value of 0 and then increment it (add 1 to it) on the following line.
+let count = 0;
+const increment = () => {
+  return (count = count + 1);
+};
+
+console.log("count on:    " + increment());
