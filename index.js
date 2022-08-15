c4b8f9a5ba6c9f065e567de16faf554aabc29a20
tuan <rickytuan1991@gmail.com>
2022-08-15 12:20:43 +0300

    lesson

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ let temp1 = "BLUE";
 console.log(temp1.toLocaleLowerCase());
 console.log(temp1.toUpperCase());
 // Charater access:
+let language = "test koe ";
+console.log(`this the result ${language[1]}`);
+console.log(`this the result ${language[3]}`);
+// length count
+
+console.log(`count the numeber ${language.length - 1}`);
+//  how to use substring:
+console.log(`this is how to use substring: ${language.substring(0, 4)}`);
+
+//
