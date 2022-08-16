4d21fb9a138fd7fe17a0920f829288780e35a4c1
tuan <rickytuan1991@gmail.com>
2022-08-16 14:16:50 +0300

    add new index_1

--- Practice_lessons1508/supermini_project01/index.js
+++ Practice_lessons1508/supermini_project01/index.js
@@ -1,44 +1,57 @@
-function sum(a, b) {}
+function sum(a, b) {
+  return a - b;
+}
 
-function subtract(a, b) {}
+function subtract(a, b) {
+  return a - b;
+}
 
-function multiply(a, b) {}
+function multiply(a, b) {
+  return (a * b).toFixed(2);
+}
 
-function divide(a, b) {}
+function divide(a, b) {
+  return (a / b).toFixed(2);
+}
 
 /* Do not touch below this line */
 let output = document.querySelector("#output");
-
+// constanst number
 let firstNumber = 0;
 let operation = null;
 let resetAfterOperation = false;
-
+//
 document.querySelectorAll("#calculator .number").forEach((button) => {
   button.addEventListener("click", (event) => {
+    // set the value for input
     let value = event.currentTarget.textContent;
+    // resetAfterOperation help  to show the outputvalue ==>  from the click button
     if (resetAfterOperation) {
       output.value = value;
       resetAfterOperation = false;
     } else {
+      console.log("value" + value);
       output.value += value;
     }
   });
 });
-
+// calculator operation => that will set the 1st number's value vs function
 document.querySelectorAll("#calculator .operation").forEach((button) => {
   button.addEventListener("click", (event) => {
+    //  set the value for 1st number:
     firstNumber = Number.parseInt(output.value);
     operation = event.currentTarget.dataset.action;
     resetAfterOperation = true;
   });
 });
-
+// equal function ==> that will set the 2nd number's value
 const equal = document.querySelector("#calculator .equal");
 equal.addEventListener("click", () => {
   if (!operation) {
     return;
   }
   resetAfterOperation = true;
+  // second Number will be the number
   let secondNumber = Number.parseInt(output.value, 10);
 
   if (operation === "sum") {
@@ -52,4 +65,12 @@ equal.addEventListener("click", () => {
   }
   //reset operation
   operation = null;
+});
+
+// reset all the operation:
+const ac = document.getElementById("ac");
+
+ac.addEventListener("click", () => {
+  output.value = 0;
+  firstNumber = 0;
 });
